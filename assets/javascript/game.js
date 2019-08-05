d07b9b2bff3bc21964c5fe2a5aadf2d5eae4ad2b
Sean Lim <lim_s@outlook.com>
2019-08-05 16:39:12 -0500

    checking answer for input letter, struggling :)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,24 @@
 const answerArray = ["tempOne", "tempTwo", "tempThree", "tempFour", "tempFive"];
 let guessedLetters = [];
+let RNG;
+let answer;
+let letterGuess;
 
-let RNG = Math.floor(Math.random() * answerArray.length);
+RNG = Math.floor(Math.random() * answerArray.length);
 
 console.log(answerArray[RNG]);
+
+answer = answerArray[RNG].toLowerCase();
+
+document.onkeyup = function(keyPress) {
+  letterGuess = keyPress.key.toString().toLowerCase();
+  accurate = letterGuess.indexOf("abcdefghijklmnopqrstuvwxyz" > -1);
+  if (accurate == true) {
+    if (answer.includes(letterGuess) == true) {
+      console.log("yes");
+    } else {
+      console.log("no");
+    }
+  } else {
+  }
+};
