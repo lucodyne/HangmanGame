6c7a48117fb0333fbc599359d1a4c185d217d3a8
Sean Lim <lim_s@outlook.com>
2019-08-05 23:55:46 -0500

    adds wrong guess items to display(dupes still allowed)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,42 @@
-const answerArray = ["tempOne", "tempTwo", "tempThree", "tempFour", "tempFive"];
-let guessedLetters = [];
+const answerArray = [
+  "BTS",
+  "SEVENTEEN",
+  "TWICE",
+  "BLACKPINK",
+  "Wanna One",
+  "EXO",
+  "Red Velvet"
+];
+let usedLetters = [];
 let RNG;
 let answer;
+let failCount = 0;
 let letterGuess;
+let letterGuessLower;
+let answerLower;
 
+// picks a random array value, converts to lower case
 RNG = Math.floor(Math.random() * answerArray.length);
 
-console.log(answerArray[RNG]);
+answer = answerArray[RNG];
+answerLower = answer.toLowerCase();
 
-answer = answerArray[RNG].toLowerCase();
+console.log(answerLower);
 
 document.onkeyup = function(keyPress) {
-  letterGuess = keyPress.key.toString().toLowerCase();
-  accurate = letterGuess.indexOf("abcdefghijklmnopqrstuvwxyz" > -1);
-  if (accurate == true) {
-    if (answer.includes(letterGuess) == true) {
-      console.log("yes");
-    } else {
-      console.log("no");
-    }
+  console.log(keyPress.key);
+  letterGuess = keyPress.key;
+  letterGuessLower = letterGuess.toLowerCase();
+  if (answerLower.includes(letterGuessLower)) {
+    console.log("yes");
   } else {
+    console.log("no");
+    failCount++;
+    const targetDiv = document.getElementById("guessPool");
+    const failDiv = document.createElement("div");
+    failDiv.textContent = letterGuessLower;
+    failDiv.className = "guessItem";
+    targetDiv.appendChild(failDiv);
   }
+  usedLetters.push(letterGuessLower);
 };
